3a792d3e2bdb715690b70a4e8cff7e51559f6d85
ahmedsamir98 <ahmedsamir64476@gmail.com>
2020-03-05 21:37:54 +0200

    add movies all models and data needed add web services for movies if you want to work on web service work on people webservice note use RX in it

--- app/src/main/java/com/example/movieapp/Utiles/CONTANTS.java
+++ app/src/main/java/com/example/movieapp/Utiles/CONTANTS.java
@@ -2,5 +2,7 @@ package com.example.movieapp.Utiles;
 
 public class CONTANTS {
     public static String APIKEY ="0b05f42616ee89883ecf6adb6af55ae0";
-    public static String IMAGEBASEURL="https://image.tmdb.org/t/p/w500";
+    public static  String IMAGEBASEURL="https://image.tmdb.org/t/p/w500";
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String LANGUAGE = "en-US";
 }
